d82a611305606eaf6296c3abb1002743ce8a933c
Sebastien Gissinger <sgissinger@gmail.com>
2025-05-18 18:10:19 +0200

    improve cicd

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,4 @@
 
-output "functionapp_url" {
-  value = "https://${azurerm_linux_function_app.functionapp.name}.azurewebsites.net"
+output "functionapp_name" {
+  value = azurerm_linux_function_app.functionapp.name
 }
